e07dcc7f171392e2d97410ac509f7f5553c25c46
Mark Dittmer <markdittmer@google.com>
2016-09-15 09:46:12 -0400

    Further tool setup and Google-style formatting in JS

--- stdlib.js
+++ stdlib.js
@@ -20,60 +20,70 @@
   define(function() {
     var stdlib = {
       argsToArray: (function() {
-        if ( typeof Array.from === 'function' )
+        if (typeof Array.from === 'function')
           return Array.from.bind(Array);
         return function argsToArray(args) {
           var array = new Array(args.length);
-          for ( var i = 0; i < args.length; i++ ) array[i] = args[i];
+          var i;
+
+          for (i = 0; i < args.length; i++) array[i] = args[i];
           return array;
         };
       })(),
       mapMap: function mapMap(srcMap, mapFn) {
         var dstMap = {};
-        var keys = Object.getOwnPropertyNames(srcMap);
-        for ( var i = 0; i < keys.length; i++ ) {
+        var keys;
+        var i;
+
+        keys = Object.getOwnPropertyNames(srcMap);
+        for (i = 0; i < keys.length; i++) {
           dstMap[keys[i]] = mapFn(srcMap[keys[i]]);
         }
         return dstMap;
       },
       multiline: function multiline(f) {
         var match = f.toString().match(/\/\*((.|\n)*?)\*\//);
-        if ( ! match ) return '';
+        if (!match) return '';
         // Within multiline comment, map "*\/" to "*/".
         return match[1].replace(/\*\\\//g, '*/');
       },
       toString: function toString(value) {
-        if ( value === null ) return null;
-        if ( value === undefined ) return undefined;
-        if ( typeof value === 'string' ) return '"' + value + '"';
+        if (value === null) return null;
+        if (value === undefined) return undefined;
+        if (typeof value === 'string') return '"' + value + '"';
         // TODO: Do all relevant platforms support Array.isArray?
-        if ( Array.isArray(value) ) return '[' + value.map(function(item) {
+        if (Array.isArray(value)) return '[' + value.map(function(item) {
           return stdlib.toString(item);
         }).join(', ') + ']';
-        return value.toString.apply(
-          value, stdlib.argsToArray(arguments).slice(1));
+        return value.toString.apply(value,
+            stdlib.argsToArray(arguments).slice(1));
       },
       getArgNames: function getArgNames(f) {
         return Function.prototype.toString.call(f).replace(
-            /\/\/.*|\/\*(.|\n)*?\*\//g, ''
-        ).match(/\((([^)]|\n)*)\)/)[1].replace(/\s+/g, '').split(',').filter(
-          function(name) { return !! name; }
-        );
+            /\/\/.*|\/\*(.|\n)*?\*\//g,
+            '').match(/\((([^)]|\n)*)\)/)[1].replace(
+                /\s+/g, '').split(',').filter(function(name) {
+                  return Boolean(name);
+                }
+            );
       },
       future: function future() {
         var value;
-        var isSet = false;
+        var isSet;
+
+        isSet = false;
         function f(f2) {
-          if ( isSet ) f2(value);
-          else         f.waiters.push(f2);
+          if (isSet) f2(value);
+          else f.waiters.push(f2);
           return f;
         }
         f.waiters = [];
         f.get = f;
         f.set = function set(v) {
+          var i;
           value = v;
           isSet = true;
-          for ( var i = 0; i < f.waiters.length; i++ ) {
+          for (i = 0; i < f.waiters.length; i++) {
             f.waiters[i](v);
           }
           isSet = false;
@@ -83,24 +93,32 @@
       },
       // Fetch URLs. Return a future that resolves after all URLs are fetched.
       loadData: function getData(urls_, opts) {
-        var urls = Array.isArray(urls_) ? urls_ : [ urls_ ];
+        var urls = Array.isArray(urls_) ? urls_ : [urls_];
+        var future;
+        var len;
+        var data;
+        var count;
+        var i;
+        var url;
+        var xhr;
+
         opts = opts || {};
 
-        var future = stdlib.future();
-        var len = urls.length;
-        var data = new Array(len);
-        var count = 0;
-        var ret;
+        future = stdlib.future();
+        len = urls.length;
+        data = new Array(len);
+        count = 0;
         function store(i) {
           data[i] = this.response;
           count++;
-          if ( count === len )
+          if (count === len)
             future.set(Array.isArray(urls_) ? data : data[0]);
         }
-        for ( var i = 0; i < len; i++ ) {
-          var url = urls[i];
-          var xhr = new XMLHttpRequest();
-          if ( opts.responseType ) xhr.responseType = opts.responseType;
+        for (i = 0; i < len; i++) {
+          url = urls[i];
+          xhr = new XMLHttpRequest();
+          if (opts.responseType)
+            xhr.responseType = opts.responseType;
           xhr.addEventListener('load', store.bind(xhr, i));
           xhr.open('GET', url);
           xhr.send();
@@ -108,10 +126,13 @@
         return future;
       },
       memo: function memo(o, key, f) {
-        var value, computed = false;
+        var value;
+        var computed;
+
+        computed = false;
         Object.defineProperty(o, key, {
           get: function() {
-            if ( computed ) return value;
+            if (computed) return value;
             value = f();
             computed = true;
             return value;
@@ -123,24 +144,28 @@
     return stdlib;
   });
 })((function() {
-  if ( typeof module !== 'undefined' && module.exports ) {
+  if (typeof module !== 'undefined' && module.exports) {
     return function(deps, factory) {
-      if ( ! factory ) module.exports = deps();
-      else             module.exports = factory.apply(this, deps.map(require));
+      if (factory)
+        module.exports = factory.apply(this, deps.map(require));
+      else
+        module.exports = deps();
     };
-  } else if ( typeof define === 'function' && define.amd ) {
+  } else if (typeof define === 'function' && define.amd) {
     return define;
-  } else if ( typeof window !== 'undefined' ) {
+  } else if (typeof window !== 'undefined') {
     return function(deps, factory) {
-      if ( ! document.currentScript ) throw new Error('Unknown module name');
+      if (document.currentScript === undefined ||
+          document.currentScript === null)
+        throw new Error('Unknown module name');
 
       window[
         document.currentScript.getAttribute('src').split('/').pop().split('#')[
           0].split('?')[0].split('.')[0]
-      ] = (factory || deps).apply(
-        this, factory ? deps.map(function(name) { return window[name]; }) : []);
+      ] = (factory || deps).apply(this, factory ? deps.map(function(name) {
+        return window[name];
+      }) : []);
     };
-  } else {
-    throw new Error('Unknown environment');
   }
+  throw new Error('Unknown environment');
 })());
